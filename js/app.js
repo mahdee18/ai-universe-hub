0656b0b2c4b5021f4bf97cd28542830d8af7765d
mahdee18 <mahdeec10@gmail.com>
2023-03-03 03:07:27 +0600

    Modal Section Commited

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ const loadFeatures = async () => {
     displayFeatures(data.data.tools)
 }
 const displayFeatures = (features) => {
-    console.log(features)
     const featuresContainer = document.getElementById('features-container')
     // Show All
     const showAll = document.getElementById('show-all')
@@ -27,9 +26,9 @@ const displayFeatures = (features) => {
                       <h3 class="card-title">Features</h3>
                     <p> 
                         <ol class="text-secondary">
-                            <li>${feature.features[0]}</li>
-                            <li>${feature.features[1]}</li>
-                            <li>${feature.features[2]}</li>
+                            <li>${feature.features[0] ? feature.features[0] : 'Data not found!!'}</li>
+                            <li>${feature.features[1] ? feature.features[1] : 'Data not found!!'}</li>
+                            <li>${feature.features[2] ? feature.features[2] : 'Data not found!!'}</li>
                         </ol>
                     </p> 
                     <hr>        
@@ -39,7 +38,7 @@ const displayFeatures = (features) => {
                         <p class="text-secondary"><i class="fa-regular fa-calendar-days"></i> ${feature.published_in}</p>
                     </div>
                     <div class="d-flex align-items-center">
-                        <button onclick="loadFeatureDetails('')" class="btn btn-light rounded-3" data-bs-toggle="modal" data-bs-target="#featureDetailsModal"><i class="fa-solid fa-arrow-right text-danger"></i></button>
+                        <button onclick="loadModal('')" class="btn btn-light rounded-3" data-bs-toggle="modal" data-bs-target="#featureDetailsModal"><i class="fa-solid fa-arrow-right text-danger"></i></button>
                     </div>
                 </div>
                     </div>
@@ -47,11 +46,39 @@ const displayFeatures = (features) => {
         `
         featuresContainer.appendChild(featureDiv)
     });
-
+    // Loader Stop
+    toggleSpinner(false)
 
 }
+const toggleSpinner = (isLoading) => {
+    const loader = document.getElementById('spinner')
+    if (isLoading) {
+        loader.classList.remove('d-none')
+    }
+    else {
+        loader.classList.add('d-none')
+    }
+}
 document.getElementById('show-all-btn').addEventListener('click', function () {
+    // Loader Start
+    toggleSpinner(true)
+
     loadFeatures()
 })
 
 loadFeatures()
+
+const loadModal = async()=>{
+    const url = (`https://openapi.programming-hero.com/api/ai/tool/01`)
+    const res = await fetch(url)
+    const data = await res.json()
+    displayModal(data.data)
+}
+const displayModal =(items)=>{
+    for (let i = 1; i <= 10; i++) {
+        loadModal(i);
+    }
+    
+}
+
+loadModal()
